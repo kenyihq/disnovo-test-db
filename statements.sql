66f5fffc46ae987d8d3f1b10e1d273e25654f5d7
Kenyi Hancco <kenyihq@gmail.com>
2022-08-10 16:50:29 -0500

    Added exercises

--- statements.sql
+++ statements.sql
@@ -49,5 +49,110 @@ SELECT * FROM resume;
 -- 2.	Mostrar por cada cliente (contact): nombre del contacto (tradename),
 -- cantidad de transacciones (tipo venta), monto total de las transacciones
 
+SELECT
+    c.trade_name AS Nombre,
+    count(tt.transaction_type_id) AS Cantidad,
+    t.transaction_total AS Total
+FROM contacts AS C,
+     transaction AS t,
+     transaction_type AS tt
+WHERE c.contact_id = t.contact_id
+    AND t.transaction_type_id = tt.transaction_type_id
+GROUP BY c.trade_name, t.transaction_total
+
+-- Creamos una vista para simplificar esta consulta para este caso usaremos una vista volatil
+
+CREATE OR REPLACE VIEW resume_total
+AS
+    SELECT
+    c.trade_name AS Nombre,
+    count(tt.transaction_type_id) AS Cantidad,
+    t.transaction_total AS Total
+    FROM contacts AS C,
+         transaction AS t,
+         transaction_type AS tt
+    WHERE c.contact_id = t.contact_id
+        AND t.transaction_type_id = tt.transaction_type_id
+    GROUP BY c.trade_name, t.transaction_total;
+ALTER TABLE resume_total
+    OWNER TO disnovo;
+
+-- Consultamos nuestra vista
+
+SELECT * FROM resume_total;
 
 
+--4.	Aumentar en un 15% el precio (price) de todos los productos
+-- cuyo tipo de producto (product_type_name) sea ‘CARNE’
+
+
+-- Como no tenemos 'CARNE' en nustra tabla sacamos el producto que mas se repite
+-- en este caso es Mace Ground, lo hacemos con la siguiente consulta
+
+SELECT count(product_type_name) cantidad, product_type_name
+FROM product_type
+GROUP BY product_type_name
+ORDER BY cantidad DESC;
+
+-- Construimos nuestra consulta, donde 'CARNE' será 'Mace Ground'
+
+SELECT
+    p.price AS precio_original,
+    to_char((p.price * 1.15)::float8, 'FM99999999.00'),
+    pt.product_type_name AS nombre
+FROM products p,
+     product_type pt
+WHERE p.product_type_id = pt.product_type_id
+AND pt.product_type_name = 'Mace Ground'
+ORDER BY precio_original DESC;
+
+--5.	 Eliminar todas las transacciones del cliente cuyo nombre (tradename) es: Juan Pérez
+
+
+-- Como generamos datos random, consultamos las que coinciden, en este caso solo tenemo 4 coincidencias
+-- en el cual 'MATERIAL SCIENCE, LCC' tiene 2 transacciones
+-- tomaremos este dato, el cual remplazara a 'Juan Perez'
+
+SELECT count(c.trade_name), c.trade_name
+FROM contacts c,
+     transaction t
+WHERE c.contact_id = t.contact_id
+GROUP BY c.trade_name
+
+-- Como tenemos el campo 'eraded' en false, cambiare este estado a true el cual nos indicará
+-- que esta borrado.
+
+-- En esta consulta mostraremos solo los que cumplan con el estado en true, el cual será igual
+-- a contactos activos con la siguientes vistas
+
+-- Creamos un procedimiento almacenado para cambiar el esatado de una transaccion
+
+CREATE OR REPLACE PROCEDURE sp_change_state(id INT)
+LANGUAGE plpgsql
+AS $$BEGIN
+
+    IF (SELECT state FROM transaction WHERE transaction_id = id) THEN
+        UPDATE transaction SET erased = true, state = false
+        WHERE transaction_id = id;
+
+    ELSE
+
+    UPDATE transaction SET erased = false, state = true
+    WHERE transaction_id = id;
+
+    END IF;
+    COMMIT;
+end;
+$$;
+
+-- Ejecutamos nuesto procedimiento almacenado
+
+CALL sp_change_state(1);
+
+-- Comprobamos que nuestro procedimeinto almacenado funcione correctamente con la siguiente consulta:
+
+SELECT erased, state FROM transaction;
+-- Volvemos a ejecutar nuestro procedimeinto almacenado logrando cambiar su estado
+CALL sp_change_state(1);
+-- Compribamos que funcione correctamente
+SELECT erased, state FROM transaction;
