d123ccb1157c579907648a68bbcde631e2b0d1ba
Kenyi Hancco <kenyihq@gmail.com>
2022-08-10 11:42:27 -0500

    Added schema

---
+++ statements.sql
@@ -0,0 +1,7 @@
+-- 1.	Mostrar de las  transacciones id_transaction, nombre del contacto (tradename),
+-- transaction_value, transaction_total , cuyo transaction_type_name=’VENTA’, fecha de
+-- la transaction (start_date_full) hayan sido realizadas en el periodo desde enero 2022
+-- hasta la fecha actual, ordenadas por tradename, fecha (start_date_full)  en forma
+-- descendente. (los montos se deben mostrar utilizando el formato 999,999,999.99 y
+-- las fecha formato: Lun, 21 Jul 2021 - 18:59
+
